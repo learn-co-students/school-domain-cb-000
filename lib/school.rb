69c42a5ab838f1da4d45cd375ec942dcfe9730ea
Kyle Greene <kgreene06@gmail.com>
2016-12-23 06:22:00 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+class School
+  attr_reader :student, :grade, :add_student, :roster, :name, :sort
+
+  def initialize(school_name)
+    @school_name= school_name
+    @roster ={}
+  end
+
+
+  def add_student(name, grade)
+    @name = name
+    if roster[grade] == nil
+      roster[grade] = [name]
+    else
+      roster[grade] << name
+    end
+  end
+
+
+  def grade(grade)
+    @grade = grade
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, student|
+      student.sort! #sorts all the students in each array and keeps them sorted with the!
+    end
+  end
+
+end
