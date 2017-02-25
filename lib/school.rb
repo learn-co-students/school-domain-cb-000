efb41f6deaad0b6f4b9901100c8d327a28dc952e
John Hillegass <cyrodil@Dragonborn.local>
2017-02-25 18:54:01 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+require 'pry'
+class School
+  attr_accessor
+  attr_reader :roster
+  attr_writer
+
+  def initialize(school_name)
+    @name = school_name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    unless @roster.include?(grade)
+      @roster[grade] = []
+    end
+    @roster[grade] << student_name
+  end
+
+  def grade(n)
+    @roster[n]
+  end
+
+  def sort
+    @roster.each do |key, value|
+      value.sort!
+    end
+    @roster
+  end
+
+end
