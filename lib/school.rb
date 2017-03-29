b439b63269b1e37ea6b9922b3084bdc2b84447e2
gstaicu <gabriel_staicu@yahoo.com>
2017-03-29 15:48:35 +0200

    Update school.rb

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+class School
+  attr_reader :roster
+
+  def initialize(school)
+    @school = school
+    @roster = Hash.new
+  end
+
+  def add_student(student_name, grade)
+    @roster.keys.include?(grade) ? @roster[grade] << student_name : @roster[grade] = [student_name]
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    sorted = {}
+    @roster.each do |grade, students|
+      sorted[grade] = students.sort
+    end
+    sorted
+  end
+
+end
