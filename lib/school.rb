9110ccee960cb04815374fcd3bd61bbbdc376e6b
Quinton Boltin <quinton@valleyexpetec.com>
2017-04-04 21:29:51 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+class School
+
+  attr_accessor :name
+  attr_reader :roster
+
+  def initialize(rstr)
+    @roster = {}
+  end
+
+  def add_student(name,grade)
+    if @roster.key?(grade)
+      @roster[grade] << name
+    else 1 + 1 == 2
+      @roster[grade] = []
+      @roster[grade] << name
+    end
+  end
+
+  def grade(num)
+    @roster[num]
+  end
+
+  def sort
+    @roster.each {|grade, name| @roster[grade].sort!}
+  end
+
+end
+
+school = School.new("Farmington")
+
+school.roster
