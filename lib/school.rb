4beae07753f4a93fbb7ca973e4942ba0059b41c0
Sergey Savrasov <ssavrass@gmail.com>
2017-04-06 11:20:15 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+
+class School
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end  
+  attr_accessor :roster
+
+  def add_student(name, grade)
+    roster[grade] = [] if roster[grade] == nil
+    roster[grade] << name
+  end
+
+  def grade(num)
+    roster[num]
+  end  
+
+  def sort
+    a = Hash[roster.sort]
+    a.values.sort!
+    a
+  end    
+
+  
+end  
