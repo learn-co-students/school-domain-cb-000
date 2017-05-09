aa5a6bce3b63781a84d9774b453bd4937aa4767d
Steven Moody <steven@beachhead.io>
2017-05-09 14:28:30 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+
+  end
+
+  def grade(level)
+    @roster[level]
+  end
+  def add_student(name, grade)
+    @roster[grade]||= []
+    @roster[grade] << name
+  end
+
+  def sort
+    #for each kv pair in object, where v is array, sort the array in place.
+    @roster[9].sort!
+    @roster
+  end
+
+end
