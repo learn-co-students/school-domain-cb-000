0a1dc687569363ddc6f159aec9d9984aed3f0e36
cptbrkfst <dyl@resonategroup.ca>
2017-06-03 15:07:33 -0400

    Completed lab

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+require 'pry'
+# This is a School class
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    roster[grade] ||= []
+    roster[grade] << name
+  end
+
+  def grade(student_grade)
+    roster[student_grade]
+  end
+
+  def sort
+    sorted_list = {}
+    roster.each do |grade, students|
+      sorted_list[grade] = students.sort
+    end
+    sorted_list
+  end
+end
