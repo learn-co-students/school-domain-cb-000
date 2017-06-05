dceeacec160418222451ea1356bf8d4854d95fb9
Tony Nassif <tonynassif@gmail.com>
2017-06-05 17:56:20 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,13 @@
-# code here!
+class School
+  attr_reader :roster
+  def initialize(new)
+    @new = new
+    @roster = {}
+  end
+  def add_student(student, grade)
+    if @roster[grade] == nil
+      @roster[grade] = []
+      @roster[grade] << student
+    end
+  end
+end
