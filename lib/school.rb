d3c768d1c38ccde42098cc821ff7bef722ad5412
Marcus Havanek <marcus.havranek@havranek.co.at>
2017-06-16 14:54:23 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @roster = Hash.new []
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(student, grade)
+    @roster[grade] += [student]
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |key, values|
+      @roster[key].sort!
+    end
+    @roster
+  end
+end
