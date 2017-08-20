2d6b82c719bbb4bfd957fbf0049e311cc46ef197
Daniel Justus <daniel.justus32@gmail.com>
2017-08-20 03:30:18 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+class School
+  ROSTER = {}
+
+  def initialize(school_name)
+    @school = school_name
+    @roster = {}
+  end
+
+  def name
+    @school
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(student, grade)
+    (@roster[grade] ||= []) << student
+  end
+
+  def grade(grade_level)
+    @roster[grade_level]
+  end
+
+  def sort
+    @roster.each do |grade, students|
+      students.sort!
+    end
+  end
+end
