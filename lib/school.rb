6253e4575ba9607199e8b329d36656d3f69201b2
Kunpeng Liu <frozenwind85@gmail.com>
2017-08-23 19:36:46 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+class School
+  attr_accessor :roster
+  attr_reader :school
+
+  def initialize(name)
+    @school = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    Hash[@roster.map { |grade, names| [grade, names.sort] }]
+
+  end
+
+end
