a440d2b91177af4126212858fe63491e86e57a6e
Uzor <uubabukoh@gmail.com>
2017-08-26 13:23:03 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  def add_student(student_name, student_grade)
+    if @roster[student_grade] == nil
+      @roster[student_grade] = []
+    end
+
+    @roster[student_grade] << student_name
+  end
+
+def grade(num)
+  return @roster[num]
+end
+
+def sort
+  for level in @roster.keys
+    @roster[level].sort!
+  end
+  return @roster
+end
+
+end
