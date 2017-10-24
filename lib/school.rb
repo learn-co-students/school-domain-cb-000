fff88d8c78c54b715c17bde90adf4f72925e6592
David Kuruvilla <dapawn@gmail.com>
2017-10-24 23:10:06 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+class School
+  attr_accessor :roster
+  attr_reader :name
+
+  def initialize  (name)
+    @name = name
+    @roster = {}
+  #  (1..12).each {|grade| roster[grade] = []}
+  end
+
+  def add_student(name,grade)
+    @roster[grade] = [] unless @roster[grade]
+    @roster[grade] << name
+  end
+
+  def grade(num)
+    @roster[num]
+  end
+
+  def sort
+    sorted = {}
+    @roster.each { |g,v|  sorted[g] = v.sort}
+    sorted
+  end
+
+end
