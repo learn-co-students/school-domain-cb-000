e2dd63a5837844b68f17e8260b963d304c94fd7c
Jen Poole <contact.vandepool@gmail.com>
2017-11-12 01:07:49 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,11 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    # initialize with a name
+    @name = name
+
+    # has an empty roster when initialized
+    @roster = {}
+  end
+end
