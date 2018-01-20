9c72bb8b83adf14e0d5ae519d81cbb70f786140f
David Hopkins <dlhopkins2@gmail.com>
2018-01-20 20:29:35 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -6,19 +6,19 @@ class School
     @roster = {}
   end
 
-  def add_student(student_name, grade)
+  def add_student(student, grade)
     roster[grade] ||= []
-    roster[grade] << student_name
+    roster[grade] << student
   end
 
-  def grade(student_grade)
-    roster[student_grade]
+  def grade(grade)
+    roster[grade]
   end
 
   def sort
     sorted = {}
-    roster.each do |grade, students|
-      sorted[grade] = students.sort
+    roster.each do |grade, student|
+      sorted[grade] = student.sort
     end
     sorted
   end
