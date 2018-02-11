68770b2620349ce6f17a58bca936c13e3ee11f97
Edwin Padron <dwnpdrn@gmail.com>
2018-02-11 07:09:05 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+# require 'pry'
+class School
+
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  # def add_student(student, grade)
+  #   @roster[grade] ||= [] << student
+  # end
+  def add_student(student, grade)
+    if @roster[grade] == nil
+      @roster[grade] = [] << student
+    else
+      @roster[grade] << student
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, students|
+      students.sort!
+    end
+  end
+end
