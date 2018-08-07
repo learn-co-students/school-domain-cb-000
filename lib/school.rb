7c13836dc7c58e4d77c962db82e669c63cea7aa6
josh.e.stroud@gmail.com Stroud <josh.e.stroud@gmail.com>
2018-08-07 21:21:28 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+class School
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(student, grade)
+    if !@roster.key?(grade)
+      @roster[grade] = [student]
+    else 
+      @roster[grade] << student
+    end
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort
+    sorted = {}
+    @roster.map do |grade, students|
+      sorted[grade] = students.sort
+    end
+    sorted
+  end
+end
