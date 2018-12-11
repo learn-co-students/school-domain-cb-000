c5a3d2337092f0f71cae0c055a7ed08465864af7
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2018-12-11 22:38:59 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+class School
+  attr_accessor :roster, :name
+
+  def initialize(school_name)
+    @name = school_name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    if @roster[grade]
+      @roster[grade] << student_name
+    else
+      @roster[grade] = []
+      @roster[grade] << student_name
+    end
+  end
+
+  def grade(num)
+    return @roster[num]
+  end
+  def sort
+    temp = @roster
+    temp.collect do |grade, students|
+      students = students.sort
+      temp[grade] = students
+    end
+    return temp
+  end
+end
+
+PHS = School.new("Princeton")
+PHS.add_student("Tomaz", 12)
+PHS.add_student("Alex", 12)
+puts PHS.sort
