62fec308b097edf375df2dfb17f2f3132c3249ce
Efua Mensah-Brown <emensahb@efbelles-laptop.hsd1.mo.comcast.net>
2019-08-26 15:12:29 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+class School
+  attr_accessor :name , :roster , :sort
+  def initialize (name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student (student_name, grade)
+    if !@roster.include?(grade)
+      @roster[grade] = []
+    end
+      @roster[grade] << student_name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    sorted = @roster.each do |grade, students|
+      students.sort!
+    end
+    sorted
+  end
+end
